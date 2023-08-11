d35c5b7c1daf475effdd4a266dd9e90e0e86b888
Sagar <creatersagar852001@gmail.com>
2023-08-11 09:39:34 +0530

    Certificate Added

--- Group -3/src/App.js
+++ Group -3/src/App.js
@@ -2,14 +2,15 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import WasteManagementServices from "./components/WasteManagementServices";
 import WasteTypeForm from "./components/WasteTypeForm";
-import segatWaste from './components/segatWaste';
-
+import SegatWaste from './components/segatWaste';
+import Certificate from './components/certificate';
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<WasteManagementServices />} />
       <Route path="/WasteTypeForm" element={<WasteTypeForm />} />
-      <Route path="/segatWaste" element={<segatWaste />} />
+      <Route path="/segatWaste" element={<SegatWaste />} />
+      <Route path="/certificate" element={<Certificate/>} />
     </Routes>
   );
 };
