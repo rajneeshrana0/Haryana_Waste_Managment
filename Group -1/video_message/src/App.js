76f29b09fd8c3f13b1e68529c99f719bc24c05c0
poornimaraghuwanshi080 <131262283+poornimaraghuwanshi080@users.noreply.github.com>
2023-08-21 00:26:42 +0530

    Update App.js

--- Group -1/video_message/src/App.js
+++ Group -1/video_message/src/App.js
@@ -1,134 +1,54 @@
-import React, { useState, useRef } from 'react';
-import { CButton, CCard, CCardBody, CCollapse, CRow, CCol } from '@coreui/react';
-// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import React, { useState, useRef} from 'react';
+import { CButton, CCard, CCardBody, CCollapse } from '@coreui/react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'; 
 import './App.css';
-import SideNav from './sidenav';
-
 import Navbar from './sidebar/Navbar';
-import Home from './pages/home';
-import Reports from './pages/reports';
-import Products from './pages/products';
 
 function App() {
-
+  const [visible, setVisible] = useState(true);
   const videoRef = useRef();
-  const [visibleA, setVisibleA] = useState(false);
-const [visibleB, setVisibleB] = useState(false);
   const handlePlay = () => {
     videoRef.current.play();
   };
   const handlePause = () => {
     videoRef.current.pause();
   };
-  
-  // slide show
- 
-  // end slide show
+  const handleLinkClick = (event) => {
+    setVisible(!visible);
+  };
+
   return (
     <>
-    
-  <link rel="stylesheet" href="C:\Users\USER\OneDrive\Documents\college internship\waste free project\style2.css"/>
+  {/* <link rel="stylesheet" href="C:\Users\USER\OneDrive\Documents\college internship\waste free project\style2.css"/> */}
   <link href="https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css" rel="stylesheet"/>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"/>
-     {/* <Router>
-        <Navbar />
-        <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/reports' component={Reports} />
-          <Route path='/products' component={Products} />
-        </Switch>
-      </Router> */}
-      
-       <SideNav/>
-       
-      {/* <div classname="sidebar">
-        <div classname="logo-details">
-          <i classname="bx bxl-c-plus-plus icon" />
-          <div classname="logo_name">Video message</div>
-          <i classname="bx bx-menu" id="btn" />
-        </div>
-        <ul classname="nav-list">
-          <li>
-            <i classname="bx bx-search" />
-            <input type="text" placeholder="Search..." />
-            <span classname="tooltip">Search</span>
-          </li>
-          <li>
-            <a href="#">
-              <i classname="bx bx-grid-alt" />
-              <span classname="links_name">Dashboard</span>
-            </a>
-            <span classname="tooltip">Dashboard</span>
-          </li>
-          <li>
-            <a href="C:/Users/admin/Desktop/minor project.html">
-              <i classname="bx bx-user" />
-              <span classname="links_name">Services</span>
-            </a>
-            <span classname="tooltip"> Services</span>
-          </li>
-          <li>
-            <a href="#">
-              <i classname="bx bx-chat" />
-              <span classname="links_name">Upload</span>
-            </a>
-            <span classname="tooltip">Upload</span>
-          </li>
-          <li>
-            <a href="C:\Users\admin\Desktop\project\client projects.html">
-              <i classname="bx bx-pie-chart-alt-2" />
-              <span classname="links_name">Broadcast system</span>
-            </a>
-            <span classname="tooltip">Broadcast system</span>
-          </li>
-          <li>
-            <a href="C:\Users\admin\Desktop\project\AboutUs.html">
-              <i classname="bx bx-folder" />
-              <span classname="links_name">Download</span>
-            </a>
-            <span classname="tooltip">Download</span>
-          </li>
-          <li>
-            <a href="C:/Users/admin/Desktop/project/registration.html">
-              <i classname="bx bx-cart-alt" />
-              <span classname="links_name">Share</span>
-            </a>
-            <span classname="tooltip">Share</span>
-          </li>
-          <li>
-            <a href="#">
-              <i classname="bx bx-cog" />
-              <span classname="links_name">Setting</span>
-            </a>
-            <span classname="tooltip">Setting</span>
-          </li>
-          <li classname="profile">
-            <div classname="profile-details">
-              <div classname="name_job">
-                <div classname="name">Poornima raghuwanshi</div>
-                <div classname="job">Web designer</div>
-              </div>
-            </div>
-            <i classname="bx bx-log-out" id="log_out" />
-          </li>
-        </ul>
-      </div> */}
-    {/* -----------main--------------- */}
+  
+ 
+  <Navbar />
+
+  
   <div className="main">
     <h1>Video Message</h1>
           <div className="container">
-  <div className="mySlides">
-  <div class="numbertext">1 / 6</div>
-        <video width="1100" height="300" ref={videoRef} >
+  <div class="mySlides">
+    <div class="numbertext">1 / 6</div>
+        <video width="1100" height="300" >
         <source src="/Videos/3.webm" />
       </video>
       </div>
-            
+      
+      <a class="prev" onclick="plusSlides(-1)">❮</a>
+  <a class="next" onclick="plusSlides(1)">❯</a>
+
       <div className="comments-likes">
               <div className="comments-section">
           <h2>Comments</h2>
           <form id="comment-form">
-            <input type="text" id="comment-input" placeholder="Write a comment..." />
+            <input
+              type="text"
+              id="comment-input"
+              placeholder="Write a comment..."
+            />
             <button type="submit">Post Comment</button>
           </form>
         </div>
@@ -149,52 +69,49 @@ const [visibleB, setVisibleB] = useState(false);
         </div>
           </div>
     </div>
-{/* ----------------------------collapse----------------------------------------------------------------------- */}
     <div className="Swachh-pledge">
-                  
-       <CRow className="row1">
-       <h4 className="panel-title">
-                  <CButton href="#collapse1" onClick={() => setVisibleA(!visibleA)}>
-                  <i className="bx bx-user" /> VIDEO INSTRUCTIONS </CButton>
-              </h4>
-      <CCol xs={6} className="Pledge-to-segregate">
-      <CCollapse visible={visibleA}>
+      <div className="panel-heading">
+        <h4 className="panel-title">
+          <CButton href="#" onClick={handleLinkClick}>
+            <i className="bx bx-user" />
+            <span className="links_name"> VIDEO INSTRUCTIONS </span>
+        
+    </CButton>
+    </h4>
+      </div>
+      <div id="collapse1" className="panel-collapse collapse">
+      <CCollapse visible={visible}>
       <CCard className="mt-3">
         <CCardBody>
-          <p>
-          Check the video format:-- Ensure your video is in MP4 or MPEG3 format.
-          <br />
-          Optimal size:-- Video should be between 50-80KB in size.
-          <br />
-          Lighting:-- Ensure proper lighting for better video quality.
-          <br />
-          Background:-- Choose an appropriate background for your video.
-          <br />
-          Eye contact:-- Maintain eye contact with the camera.
-          <br />
-          Audio quality:-- Ensure high-quality audio for clear sound.
-          <br />
-          Dress appropriately:-- Dress according to the context of the video.
-          <br />
-          Participant interaction:-- Follow the ground rules for interactions.
-          </p>
+      <ol class="instruction-list">
+          <li>Check the video format: Ensure your video is in MP4 or MPEG3 format.</li>
+          <li>Optimal size: Video should be between 50-80KB in size.</li>
+          <li>Lighting: Ensure proper lighting for better video quality.</li>
+          <li>Background: Choose an appropriate background for your video.</li>
+          <li>Eye contact: Maintain eye contact with the camera.</li>
+          <li>Audio quality: Ensure high-quality audio for clear sound.</li>
+          <li>Dress appropriately: Dress according to the context of the video.</li>
+          <li>Participant interaction: Follow the ground rules for interactions.</li>
+        </ol>
         </CCardBody>
       </CCard>
     </CCollapse>
-    </CCol>
-    </CRow>
-    </div> 
+      </div>
+    </div>
     <div className="Pledge-to-segregate">
-      <CButton href="#collapse2" onClick={() => setVisibleB(!visibleB)}>
+      <div className="panel-heading">
+      <CButton href="#" onClick={handleLinkClick}>
         <h4 className="panel-title">
+          <a data-toggle="collapse" href="#collapse2">
             <i className="bx bx-cart-alt" />
             <span className="links_name">DO'S AND DON'Ts</span>
-                   </h4>
+          </a>
+          </h4>
       </CButton>
         
-      <CRow className="row2">
-        <CCol xs={6} className="Pledge-to-segregate">
-      <CCollapse visible={visibleB}>
+      </div>
+      <div id="collapse2" className="panel-collapse collapse">
+      <CCollapse visible={visible}>
       <CCard className="mt-3">
         <CCardBody>
         <p>
@@ -211,11 +128,8 @@ const [visibleB, setVisibleB] = useState(false);
         </CCardBody>
       </CCard>
     </CCollapse>
-    </CCol>
-    </CRow> 
+      </div>
     </div>
-          </div>
-   
     <section>
       <nav>
         <div className="total1">
@@ -236,7 +150,8 @@ const [visibleB, setVisibleB] = useState(false);
         </div>
       </nav>
     </section>
-    <div classeName="products" id="products">
+
+    <div className="products" id="products">
       <h1>swachh Abhiyan</h1>
       <div className="box">
         <div className="card">
@@ -244,7 +159,7 @@ const [visibleB, setVisibleB] = useState(false);
             <img src={require('./images/download (1).jpg')} alt="example" />
           </div>
           <div className="products_text">
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <p>Swachh Bharat urban toilet plan is 76 percent behind schedule.</p>
             <h3>location:</h3>
           </div>
         </div>
@@ -253,7 +168,7 @@ const [visibleB, setVisibleB] = useState(false);
             <img src={require('./images/download (2).jpg')} alt="example" />
           </div>
           <div className="products_text">
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <p>Swachh Bharat: A public health crisis is upon us; here is how to avert it</p>
             <h3>location:</h3>
           </div>
         </div>
@@ -262,7 +177,7 @@ const [visibleB, setVisibleB] = useState(false);
             <img src={require('./images/download (3).jpg')} alt="example" />
           </div>
           <div className="products_text">
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <p>DM hits streets to check trash disposal.</p>
             <h3>location:</h3>
           </div>
         </div>
@@ -271,7 +186,7 @@ const [visibleB, setVisibleB] = useState(false);
             <img src={require('./images/download (4).jpg')} alt="example" />
           </div>
           <div className="products_text">
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <p>West Bengal government starts special cleanliness drive.</p>
             <h3>location:</h3>
           </div>
         </div>
@@ -280,7 +195,7 @@ const [visibleB, setVisibleB] = useState(false);
             <img src={require('./images/download (5).jpg')} alt="example" />
           </div>
           <div className="products_text">
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <p>Consolidated day-wise report on Swachhata Hi Seva 2018 pertaining to ICAR.</p>
             <h3>location:</h3>
           </div>
         </div>
@@ -298,7 +213,7 @@ const [visibleB, setVisibleB] = useState(false);
             <img src={require('./images/download (7).jpg')} alt="example" />
           </div>
           <div className="products_text">
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <p>A civic tech approach to Swachh Bharat</p>
             <h3>location:</h3>
           </div>
         </div>
@@ -307,12 +222,13 @@ const [visibleB, setVisibleB] = useState(false);
             <img src={require('./images/download (8).jpg')} alt="example" />
           </div>
           <div className="products_text">
-            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+            <p>                Govt issues report on Swachh Bharat Mission Urban</p>
             <h3>location:</h3>
           </div>
         </div>
       </div>
     </div>
+  </div>
 </>
 
   );
